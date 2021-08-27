daa29398c69654f0a38b8cb40ace56ea31a6cadd
Mick Jasker <mick.jasker@mediamonks.com>
2021-08-27 14:08:29 +0200

    Set up support libraries

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    '@mediamonks/eslint-config-base',
+  ],
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  ignorePatterns: ['src/server-bundle.ts', 'config', 'src/polyfills.js', 'dist'],
+  rules: {
+    // Additions
+    '@typescript-eslint/consistent-type-imports': ['error'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['strictCamelCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        selector: 'typeLike',
+        format: ['StrictPascalCase'],
+      },
+      {
+        selector: 'variable',
+        // Exception for FunctionComponents
+        format: ['strictCamelCase', 'StrictPascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'function',
+        // Exception for FunctionComponents
+        format: ['strictCamelCase', 'StrictPascalCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ['StrictPascalCase'],
+      },
+    ],
+  },
+};

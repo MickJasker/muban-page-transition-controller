f86720643a3fdd9480770d878e491ef9651a79e5
Mick Jasker <mick.jasker@mediamonks.com>
2021-09-03 13:57:28 +0200

    Remove Axios and replace it native fetch

--- src/lib/fetchDocument.ts
+++ src/lib/fetchDocument.ts
@@ -1,12 +1,9 @@
-import axios from 'axios';
-
 /**
  * Fetch the new DOM from the provided URL
  *
  * @param {URL} url URL of the new page
  * @return Promise<Document> The DOM of the fetched page
  * */
-export const fetchDocument = async (url: string): Promise<Document> => {
-  const response = await axios.get<string>(url);
-  return new DOMParser().parseFromString(response.data, 'text/html');
-};
+export const fetchDocument = async (url: string): Promise<Document> =>
+  new DOMParser()
+  .parseFromString((await (await fetch(url)).text()), 'text/html');
